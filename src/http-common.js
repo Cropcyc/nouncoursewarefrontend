ddc678d86f86eab3ff1994763adab22ae9196f99
Babajide Olapade <babajidelapade@gmail.com>
2022-01-17 04:07:21 +0100

    baseURL herokuapp.com/api/courses

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://fierce-inlet-25199.herokuapp.com/#/add',
+    baseURL: 'https://fierce-inlet-25199.herokuapp.com/api/courses',
     headers: {
         'Content-type': 'application/json'
     }
