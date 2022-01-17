b94477649bc7cb7f4fc5de4d17daded0d9b8cc35
Babajide Olapade <babajidelapade@gmail.com>
2022-01-17 03:34:31 +0100

    axios baseurl changed to herokuapp.com/#/add

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://fierce-inlet-25199.herokuapp.com/api',
+    baseURL: 'https://fierce-inlet-25199.herokuapp.com/#/add',
     headers: {
         'Content-type': 'application/json'
     }
